74815d23f15317797133310a344bd8391f2e0085
Mutaz-Alqaimary <mutazalqaimary5@gmail.com>
2026-06-08 23:23:59 +0300

    Add babel-plugin-react-compiler and enable reactCompiler in Next.js config

--- components/animations/scroll-progress.tsx
+++ components/animations/scroll-progress.tsx
@@ -6,5 +6,5 @@ export function ScrollProgress() {
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, { stiffness: 180, damping: 26 });
 
-  return <motion.div className="fixed left-0 top-0 z-50 h-1 w-full origin-left bg-primary" style={{ scaleX }} />;
+  return <motion.div className="fixed left-0 top-0 z-50 h-1 w-full origin-left bg-primary lg:hidden" style={{ scaleX }} />;
 }
